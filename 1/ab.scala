aa0c751ad97eaf2a444fd17c23b54c93055d4ceb
Ebbe Flisbäck <ebbe.flisback@gmail.com>
2024-12-01 22:35:56 +0100

    Day 1, zipping in a

--- 1/ab.scala
+++ 1/ab.scala
@@ -5,25 +5,23 @@
   println(b(lines))
 }
 
-def parseColumns(lines: Iterable[String]): (Vector[Int], Vector[Int]) = {
+def parseColumns(lines: Iterable[String]) = {
   (for line <- lines yield
     val numbers = line.split("   ").map(_.toInt)
     (numbers(0), numbers(1))
   ).toVector.unzip
 }
 
-def a(lines: Iterable[String]): Int = {
-  val (leftSorted, rightSorted) = parseColumns(lines) match
-    case (left, right) => (left.sorted, right.sorted)
+def a(lines: Iterable[String]) = {
+  val pairs = parseColumns(lines) match
+    case (left, right) => (left.sorted zip right.sorted)
 
-  (for i <- 0 until leftSorted.length
-  yield math.abs(leftSorted(i) - rightSorted(i))).sum
+  (for pair <- pairs yield math.abs(pair(0) - pair(1))).sum
 }
 
-def b(lines: Iterable[String]): Int = {
+def b(lines: Iterable[String]) = {
   val (left, right) = parseColumns(lines)
 
   val frequencyMap = right.groupBy(identity).view.mapValues(_.size)
-  (for number <- left yield
-    number * frequencyMap.getOrElse(number, 0)).sum
+  (for number <- left yield number * frequencyMap.getOrElse(number, 0)).sum
 }
