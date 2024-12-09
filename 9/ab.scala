abb7f7a8b2cbd227615055a808507a0f3f057492
Ebbe Flisbäck <ebbe.flisback@gmail.com>
2024-12-09 15:32:46 +0100

    Day 9, prettier but still not working solution

--- 9/ab.scala
+++ 9/ab.scala
@@ -1,10 +1,10 @@
+type Block = Option[Int]
+
 @main def run =
   val input = io.Source.fromFile("input.txt").getLines.next
-  println("Input: " + input)
+  println("a: " + a(input))
 
-  println("Sum: " + a(input))
-
-def getBlocks(input: String): Array[Option[Int]] =
+def getBlocks(input: String) =
   input.toCharArray.zipWithIndex
     .flatMap((c, i) =>
       if (i % 2 == 0) Array.fill(c.asDigit)((Some(i / 2)))
@@ -12,43 +12,54 @@ def getBlocks(input: String): Array[Option[Int]] =
     )
     .toArray
 
-def getFreeIndexes(blocks: Iterable[Option[Int]]) =
+def getFreeIndexes(blocks: Iterable[Block]) =
   (for
     (maybeInt, i) <- blocks.zipWithIndex
     if !maybeInt.isDefined
   yield i).toArray
 
-def printBlocks(blocks: Array[Option[Int]]) =
-  for maybeInt <- blocks do
-    if maybeInt.isDefined then print("[" + maybeInt.get + "]") else print('.')
-  println()
+def printBlocks(blocks: Array[Block]) =
+  for maybeInt <- blocks do print("[" + maybeInt.getOrElse('.') + "]")
+  println
 
 def getRearrangedBlocks(
-    blocks: Array[Option[Int]],
-    freeIndexes: Array[Int]
-): Array[Option[Int]] =
-  printBlocks(blocks)
-  if freeIndexes.isEmpty then return blocks
+    blocks: Array[Block],
+    freeIndexes: Array[Int],
+    debug: Boolean
+): Array[Block] =
+  if debug then printBlocks(blocks)
+  if freeIndexes.isEmpty then 
+    if debug then println
+    return blocks
 
   val modifiedBlocks = blocks.clone()
 
   val lastIndex = blocks.lastIndexWhere(_.isDefined)
   val swapIndex = freeIndexes.head
 
-  if swapIndex > lastIndex then return blocks
+  if swapIndex > lastIndex then 
+    if debug then println
+    return blocks
 
   val temp = modifiedBlocks(lastIndex)
   modifiedBlocks(lastIndex) = modifiedBlocks(swapIndex)
   modifiedBlocks(swapIndex) = temp
 
-  getRearrangedBlocks(modifiedBlocks, freeIndexes.tail)
+  getRearrangedBlocks(modifiedBlocks, freeIndexes.tail, debug)
 
-def a(input: String) =
+def a(input: String, debug: Boolean = false) =
   val blocks = getBlocks(input)
-  printBlocks(blocks)
   val freeIndexes = getFreeIndexes(blocks)
-  println("Free indexes: " + freeIndexes.mkString(", "))
-  println("----------")
-  getRearrangedBlocks(blocks, freeIndexes).zipWithIndex
-    .map((maybeInt, i) => if maybeInt.isDefined then maybeInt.get * i else 0)
+
+  if debug then 
+    println("Input: " + input)
+    printBlocks(blocks)
+    println("Free indexes: " + freeIndexes.mkString(", "))
+    println
+  
+  getRearrangedBlocks(blocks, freeIndexes, debug).zipWithIndex
+    .map((maybeInt, i) => maybeInt.getOrElse(0) * i)
     .sum
+
+// Wrong answers
+// 829320760
