297a9b8582d766559078001661e097dd335f1b6e
Ebbe Flisbäck <ebbe.flisback@gmail.com>
2024-12-09 13:55:36 +0100

    Day 9, handle multiple-digit indexes

--- 9/ab.scala
+++ 9/ab.scala
@@ -4,35 +4,38 @@
 
   println("Sum: " + a(input))
 
-def getBlocks(input: String) = input.toCharArray.zipWithIndex
-  .flatMap((c, i) =>
-    if (i % 2 == 0) Array.fill(c.asDigit)((i / 2).toString.head)
-    else Array.fill(c.asDigit)('.')
-  )
-  .toArray
+def getBlocks(input: String): Array[Option[Int]] =
+  input.toCharArray.zipWithIndex
+    .flatMap((c, i) =>
+      if (i % 2 == 0) Array.fill(c.asDigit)((Some(i / 2)))
+      else Array.fill(c.asDigit)(None)
+    )
+    .toArray
 
-def getFreeIndexes(blocks: Iterable[Char]) =
+def getFreeIndexes(blocks: Iterable[Option[Int]]) =
   (for
-    (char, i) <- blocks.zipWithIndex
-    if char == '.'
+    (maybeInt, i) <- blocks.zipWithIndex
+    if !maybeInt.isDefined
   yield i).toArray
 
+def printBlocks(blocks: Array[Option[Int]]) =
+  for maybeInt <- blocks do
+    if maybeInt.isDefined then print("[" + maybeInt.get + "]") else print('.')
+  println()
+
 def getRearrangedBlocks(
-    blocks: Array[Char],
+    blocks: Array[Option[Int]],
     freeIndexes: Array[Int]
-): Array[Char] =
-  if freeIndexes.isEmpty then 
-    println("Rearranged blocks: " + blocks.mkString)
-    return blocks
+): Array[Option[Int]] =
+  printBlocks(blocks)
+  if freeIndexes.isEmpty then return blocks
 
   val modifiedBlocks = blocks.clone()
 
-  val lastIndex = blocks.lastIndexWhere(_ != '.')
+  val lastIndex = blocks.lastIndexWhere(_.isDefined)
   val swapIndex = freeIndexes.head
 
-  if swapIndex > lastIndex then 
-    println("Rearranged blocks: " + blocks.mkString)
-    return blocks
+  if swapIndex > lastIndex then return blocks
 
   val temp = modifiedBlocks(lastIndex)
   modifiedBlocks(lastIndex) = modifiedBlocks(swapIndex)
@@ -42,8 +45,10 @@ def getRearrangedBlocks(
 
 def a(input: String) =
   val blocks = getBlocks(input)
-  println("Blocks: " + blocks.mkString)
+  printBlocks(blocks)
   val freeIndexes = getFreeIndexes(blocks)
+  println("Free indexes: " + freeIndexes.mkString(", "))
+  println("----------")
   getRearrangedBlocks(blocks, freeIndexes).zipWithIndex
-    .map((c, i) => if c != '.' then c.asDigit * i else 0)
+    .map((maybeInt, i) => if maybeInt.isDefined then maybeInt.get * i else 0)
     .sum
